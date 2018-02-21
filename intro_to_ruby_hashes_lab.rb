0217c184b05724301b49b92982b9895e0f1ddeec
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-21 21:16:49 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,19 +50,21 @@ def monopoly_with_fourth_tier
 	monopoly = {
 		:railroads => {
 			:pieces => 4,
+			
 			:names => {
 				:reading_railroad => {},
 				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline_railroad => {}
-			},
+			} #end names,
+			
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
 				:three_pieces_owned => 100,
 				:four_pieces_owned => 200
-			}
-		}
-	}
+			} #end rent_in_dollars
+		} #end railroads
+	} #end monopoly
 
 end
